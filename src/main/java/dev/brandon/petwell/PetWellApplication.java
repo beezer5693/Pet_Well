4db1b8221b7d8d8f829b2d26c7d4356845be9eba
Brandon Bryan <bshwag@gmail.com>
2024-03-17 19:19:12 -0400

    Initial commit

---
+++ src/main/java/dev/brandon/petwell/PetWellApplication.java
@@ -0,0 +1,13 @@
+package dev.brandon.petwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetWellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetWellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/brandon/petwell/PetWellApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.brandon.petwell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetWellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
